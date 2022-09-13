417ef8c8bd7989af78382c8696014bb556730552
jrtcmx <104660521+jrt-cmx@users.noreply.github.com>
2022-09-13 22:11:38 +0800

    Display  update
    main

--- flaskr/display.py
+++ flaskr/display.py
@@ -1,8 +1,21 @@
 from flask import Blueprint, request, render_template
 
+
 display = Blueprint('display', __name__)
 
 
 @display.route("/")
-def home():
+def displayHome():
   return render_template('home.html')
+
+
+def displayStock():
+  return 'hi'
+
+def search ():
+  # Get search symbol
+  search_symbol = request.form.get('search_symbol')
+    
+
+  return displayStock(search_symbol)
+  
